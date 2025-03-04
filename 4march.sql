198dbc6036a92143e043172807744d0443e35bd5
unknown <cherrysingla2466@gmail.com>
2025-03-04 23:10:58 +1100

    practice

---
+++ 4march.sql
@@ -0,0 +1,13 @@
+Create database Australia
+
+create table employee ( worker_id int, name varchar, position varchar, salary int, timing int, room varchar)
+begin tran
+
+select * from employee where ID = 1
+update employee SET 
+
+select * from employee 
+
+insert into employee ( worker_id, name, position, salary, timing, room )
+
+values ( '11', 'abcd', 'head', '2000000', '7am', 'engineer')
