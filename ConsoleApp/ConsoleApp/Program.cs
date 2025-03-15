8cdab816949d27f9d791ee3ac91d86291cb1bee9
Prince-Coding-Lab <princeofficial88@gmail.com>
2025-03-15 07:23:11 +0530

    Commit console app

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Program 1 for age calculation
+//Console.WriteLine("Please enter your age:");
+//int age = Convert.ToInt32(Console.ReadLine());
+
+
+
+//if (age > 35)
+//	Console.WriteLine("You are very old");
+//else if (age<10)
+//	Console.WriteLine("You are a child");
+//else 
+//	Console.WriteLine("You are very young");
+
+
+//Program to print full name 
+
+//Console.WriteLine("Please enter your firstName");
+//string firstName = Console.ReadLine();
+
+//Console.WriteLine("Plese enter your lastName");
+//string lastName = Console.ReadLine();
+
+//Console.WriteLine("Please enter your email address");
+//string email = Console.ReadLine();
+
+//Console.WriteLine("Your full name is: " + firstName + " " + lastName);
+//Console.WriteLine("Your email address is: " + email);
+
+
+//Create a program to sum of two number
+
+Console.WriteLine("Please enter first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = number1 + number2;
+
+Console.WriteLine("Sum is: " + sum);
+
+//string firstName = "";
+
+
+//firstName = "Prince";
+//int pageNumber = 50;
+//double pertage = 50.5;
