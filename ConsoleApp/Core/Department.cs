dd20209d51c402b8d7cef282d491f74632cec7e2
Prince-Coding-Lab <princeofficial88@gmail.com>
2025-04-04 03:12:42 +0530

    Project library added

---
+++ ConsoleApp/Core/Department.cs
@@ -0,0 +1,37 @@
+﻿namespace Core
+{
+	public class Department
+	{
+		//field members
+		private int id;
+
+		private string name; // field
+
+		public string Name   // property
+		{
+			get { return name; }   // get method
+			set { name = value; }  // set method
+		}
+
+		//properties
+		public int ID { get; set; }
+
+		//default constructors
+		public Department()
+		{
+
+		}
+
+		// parametere constructors
+		public Department(int id)
+		{
+			this.ID = id;
+		}
+
+		//Methods
+		public void Method1()
+		{ 
+		
+		}
+	}
+}
